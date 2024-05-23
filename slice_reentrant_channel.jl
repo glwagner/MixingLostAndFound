8b497f6874ff8bf843b77ab6ee055dafe3167cd1
Gregory Wagner <wagner.greg@gmail.com>
2024-05-23 13:07:03 -0600

    Plot zonal velocity

--- slice_reentrant_channel.jl
+++ slice_reentrant_channel.jl
@@ -8,7 +8,26 @@ Nx = 1
 Ny = 120
 Nz = 42
 
-Lz = 5000
+Δz₀ = 10.0 # m
+Δz = [Δz₀]
+Lz = Δz₀
+ϵ = 1.026 # yields Nz = 42
+Δzk = Δz₀
+z = [-Δz₀]
+
+while Lz < 5000
+    global Δzk, Lz
+    Δzk = Δzk^ϵ
+    push!(Δz, Δzk)
+    push!(z, z[end] - Δzk)
+    Lz = Lz + Δzk
+end
+
+z = reverse(z)
+push!(z, 0)
+Δz = reverse(Δz)
+Nz = length(Δz)
+
 κz = 1e-6
 T₀ = 30 # degrees
 τ★ = 10days # surface restoring timescale
@@ -23,7 +42,7 @@ filename = "slice_reentrant_channel.jld2"
 grid = LatitudeLongitudeGrid(size = (Nx, Ny, Nz),
                              longitude = (-10, 10),
                              latitude = (-60, 60),
-                             z = (-Lz, 0),
+                             z = z,
                              topology = (Periodic, Bounded, Bounded))
 
 vitd = VerticallyImplicitTimeDiscretization()
@@ -51,34 +70,48 @@ set!(model, T=T₀)
 
 simulation = Simulation(model, Δt=10minutes, stop_time=30days)
 
-output_writer = JLD2OutputWriter(model, model.tracers; filename,
-                                 schedule = TimeInterval(10days),
+outputs = merge(model.velocities, model.tracers)
+output_writer = JLD2OutputWriter(model, outputs; filename,
+                                 schedule = TimeInterval(1days),
                                  overwrite_existing = true)
 
 simulation.output_writers[:jld2] = output_writer
+
+function progress(sim)
+    msg = string("Iter: ", iteration(sim), ", time: ", prettytime(sim))
+    @info msg
+    return nothing
+end
+
+add_callback!(simulation, progress, IterationInterval(100))
 
 run!(simulation)
 
 # Analyze results
 
 Tt = FieldTimeSeries(filename, "T")
+ut = FieldTimeSeries(filename, "u")
 t = Tt.times
 Nt = length(Tt)
-z = znodes(Tt)
-φ = φnodes(Tt)
+λ, φ, z = nodes(Tt)
 
 fig = Figure()
 axT = Axis(fig[1, 1], xlabel="Latitude (ᵒ)", ylabel="z (m)")
-slider = Slider(fig[2, 1], startvalue=1, range=1:Nt)
+axu = Axis(fig[1, 2], xlabel="Latitude (ᵒ)", ylabel="z (m)")
+slider = Slider(fig[2, 1:2], startvalue=1, range=1:Nt)
 n = slider.value
 
 Tn = @lift interior(Tt[$n], 1, :, :)
 heatmap!(axT, φ, z, Tn)
 
-title = @lift string("Temperature after ", prettytime(t[$n]))
-Label(fig[0, 1], title, tellwidth=false)
+un = @lift interior(ut[$n], 1, :, :)
+heatmap!(axu, φ, z, un)
 
-ylims!(axT, -100, 0)
+title = @lift string("Temperature after ", prettytime(t[$n]))
+Label(fig[0, 1:2], title, tellwidth=false)
+
+ylims!(axT, -200, 0)
+ylims!(axu, -200, 0)
 
 display(fig)
 
